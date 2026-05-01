c082856d7ceffdde6156d70278daec866159ae6d
Shital Undalkar <156774238+shitalundalkar@users.noreply.github.com>
2026-05-01 19:28:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,89 @@
+import pickle
+import streamlit as st
+import requests
+import time
+
+# ---------- Fetch Poster Safely ----------
+def fetch_poster(movie_id):
+    try:
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+        response = requests.get(url, timeout=5)
+
+        if response.status_code != 200:
+            return "https://via.placeholder.com/500x750?text=No+Image"
+
+        data = response.json()
+        poster_path = data.get('poster_path')
+
+        if poster_path:
+            return "https://image.tmdb.org/t/p/w500/" + poster_path
+        else:
+            return "https://via.placeholder.com/500x750?text=No+Image"
+
+    except:
+        return "https://via.placeholder.com/500x750?text=Error"
+
+
+# ---------- Recommendation Logic ----------
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(
+        list(enumerate(similarity[index])),
+        reverse=True,
+        key=lambda x: x[1]
+    )
+
+    recommended_movie_names = []
+    recommended_movie_posters = []
+
+    for i in distances[1:6]:
+        movie_id = movies.iloc[i[0]].movie_id
+
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+
+        time.sleep(0.2)  # prevent API blocking
+
+    return recommended_movie_names, recommended_movie_posters
+
+
+# ---------- UI ----------
+st.header('Movie Recommender System 🎬')
+
+movies = pickle.load(open('movie_list.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+movie_list = movies['title'].values
+
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
+
+if st.button('Show Recommendation'):
+
+    with st.spinner('Fetching recommendations...'):
+        names, posters = recommend(selected_movie)
+
+    # NEW columns (not beta)
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
